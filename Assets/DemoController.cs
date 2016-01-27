a21004aabefe676c58469340fd3051a760e000a8
Alexander Ivanov <alexander.ivanov.1989@outlook.com>
2016-01-27 17:52:28 +0200

    first commit

---
+++ Assets/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoController : MonoBehaviour {
+	private Animator animator;
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+		animator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+	}
+}
